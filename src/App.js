03ac22f0c321c3371bf43e4eaf1c0366fae2e592
hebah al daealieb <hebah20082gmail.com>
2021-09-13 14:54:33 +0300

    edit weather

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import Card from "react-bootstrap/Card";
 
-// import Movies from "./component/Movies";
 export class App extends Component {
   constructor(props) {
     super(props);
@@ -11,8 +10,6 @@ export class App extends Component {
       error: false,
       locationImg: "",
       locationInfos: [],
-      locationMovie: [],
-
     };
   }
   submitForm = async (e) => {
@@ -27,13 +24,13 @@ export class App extends Component {
       this.setState({
         locationData: response.data[0],
       });
-      const response2 = await axios.get (
+      const response2 = await axios.get(
         `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${this.state.locationData.lat},${this.state.locationData.lon}&format=jpg `
       );
       
 
       this.setState({
-        error: false ,
+        error: false,
         locationImg: response2.config.url,
       });
 
@@ -43,23 +40,17 @@ export class App extends Component {
       const response3 = await axios.get(
         `${process.env.REACT_APP_server_url}/weathers?city=${location}`
       );
-// console.log(response3)
+console.log(response3)
       this.setState({
         locationInfos: response3.data,
       });
 //to
-const response4 = await axios.get(
-  `${process.env.REACT_APP_server_url}/movies?query=${location}`
-);
-
-this.setState({
-  locationMovie: response4.data,
-});
-// console.log(respones4);
 
 
 
-      // console.log("our cs response", this.state.locationMovie);
+
+
+      console.log("our cs response", this.locationImg);
     } catch (error) {
       console.log("catch error" + error);
       this.setState({
@@ -110,21 +101,23 @@ this.setState({
                 })}
 
 
+
+
+
+
+
               </Card.Body>
             </Card>
             <img src={this.state.locationImg} alt={""} />
           </div>
-          
         }
-
-
         <div>
           {this.state.error && <p style={{ color: "white", }}>Location not found try again </p>}
           {this.state.locationData.lon && <p style={{ color: "white", }} >The Selected Map </p>}
 
         </div>
         <div >
-        {/* <Movies locationMovie={this.state.locationMovie} /> */}
+
         </div>
       </div>
     );
